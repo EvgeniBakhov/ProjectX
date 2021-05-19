09055d278a874565369696e30fc6075b98829cd2
Yevhen Bakhov <ostneel@gmail.com>
2021-05-20 02:50:31 +0300

    Added empty endpoints for reservation

--- src/main/java/com/projectx/ProjectX/controller/ReservationController.java
+++ src/main/java/com/projectx/ProjectX/controller/ReservationController.java
@@ -1,12 +1,60 @@
 package com.projectx.ProjectX.controller;
 
+import com.projectx.ProjectX.exceptions.EntityNotFoundException;
+import com.projectx.ProjectX.model.User;
+import com.projectx.ProjectX.model.resource.ReservationResponseResource;
 import com.projectx.ProjectX.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/reservation")
 public class ReservationController {
 
     @Autowired
     ReservationService reservationService;
+
+    @PostMapping(value = "/{eventId}")
+    public ResponseEntity createReservation(@PathVariable Long eventId,
+                                            @AuthenticationPrincipal User user) {
+        try {
+            ReservationResponseResource reservation = reservationService.createReservation(eventId, user);
+            return ResponseEntity.ok().body(reservation);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PutMapping(value = "/{eventId}/cancel")
+    public ResponseEntity cancelReservation() {
+        return null;
+    }
+
+    @PutMapping(value = "/{eventId}/approve")
+    public ResponseEntity approveReservation() {
+        return null;
+    }
+
+    @GetMapping(value = "/{reservationId}")
+    public ResponseEntity getReservationById() {
+        return null;
+    }
+
+    @GetMapping()
+    public ResponseEntity findReservationsForCurrentUser(@AuthenticationPrincipal User user) {
+        return null;
+    }
+
+    @GetMapping(value = "/event/{eventId}")
+    public ResponseEntity findReservationForEvent(@PathVariable Long eventId, @AuthenticationPrincipal User user) {
+        return null;
+    }
+
+    @GetMapping(value = "user/{userId}")
+    public ResponseEntity findReservationsByUserId(@PathVariable Long userId, @AuthenticationPrincipal User user) {
+        return null;
+    }
 }
